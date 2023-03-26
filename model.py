4091e8e4beeab403daf10da8333d1fb88a40a7d1
michellechang02 <changmi@seas.upenn.edu>
2023-03-26 17:44:00 -0400

    changed

--- model.py
+++ model.py
@@ -1,15 +1,15 @@
-import json
-from gensim.models import Doc2Vec, TaggedDocument
-from gensim.utils import simple_preprocess
-from sklearn.model_selection import train_test_split
 import collections
 import re
 import requests
+from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+from gensim.utils import simple_preprocess
+from sklearn.model_selection import train_test_split
+
 
 # function to make TaggedDocuments for training. set tags to article name (can also use id)
-def create_tagged_document(articles):
-    for i, article in enumerate(articles):
-        yield TaggedDocument(words=simple_preprocess(article['content']), tags=article['name'])
+def create_tagged_document(article):
+    return TaggedDocument(words=simple_preprocess(article['content']), tags=article['slug'])
 
 #load json files
 SECTIONS = ['news', 'sports', 'opinion']
@@ -26,25 +26,24 @@ def scrape_data():
        for page in range(PAGES):
            resp = requests.get(f'https://www.thedp.com/section/{section}.json?page={page + 1}').json()
            articles = resp['articles']
+           print(len(articles))
            for article in articles:
                 cleaned_article = clean_text(article['content'])
-                articles = json.load(cleaned_article)
-                tagged_articles.append()
+                print(type(cleaned_article))
+                tagged_articles.append(create_tagged_document(cleaned_article))
 
-
-tagged_articles = list(create_tagged_document(articles))
-
+print(len(tagged_articles))
 # split data
 train_articles, test_articles = train_test_split(tagged_articles, test_size=0.2)
 
 # train the Doc2Vec - need to tweak vector_size hyperparameter
 model = Doc2Vec(vector_size=50, min_count=3, epochs=30)
-model.build_vocab(articles)
+model.build_vocab(train_articles)
 model.train(tagged_articles, total_examples=model.corpus_count, epochs=model.epochs)
 
 # are we just loading the tagged vectors to the db, or should we run like KNN or KMeans ML model?
 train_vectors = [model.infer_vector(article.words) for article, _ in train_articles]
-test_vectors = [model.infer_vector(articles.words) for article, _ in test_articles]
+test_vectors = [model.infer_vector(article.words) for article, _ in test_articles]
 
 ranks = []
 second_ranks = []
