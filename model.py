6c60639e2369bbb27708a9c790749f653d7ccf81
kriyip <kyip719@gmail.com>
2023-04-16 16:21:05 -0400

    get max number of pages

--- model.py
+++ model.py
@@ -6,14 +6,14 @@ from gensim.models.doc2vec import TaggedDocument
 from gensim.utils import simple_preprocess
 from sklearn.model_selection import train_test_split
 
+
 # function to make TaggedDocuments for training. set tags to article name (can also use id)
-def create_tagged_document(article, tags):
-    return TaggedDocument(words=simple_preprocess(article), tags={tags})
+def create_tagged_document(article):
+    return TaggedDocument(words=simple_preprocess(article['content']), tags=article['slug'])
 
 #load json files
 SECTIONS = ['news', 'sports', 'opinion']
-PAGES = 1
-ids = []
+PAGES = 10
 
 def clean_text(txt):
     clean = re.compile('(<.*?>)|(&nbsp;)|(&amp;)')
@@ -21,35 +21,41 @@ def clean_text(txt):
 
 tagged_articles = []
 
+
 def scrape_data():
     for section in SECTIONS:
-       for page in range(PAGES):
+       # get max number of pages
+       resp = requests.get(f'https://www.thedp.com/section/{section}.json?page=1').json()
+       num_pages = resp['pagination']['last']
+       
+       for page in range(1, num_pages+1):
            resp = requests.get(f'https://www.thedp.com/section/{section}.json?page={page + 1}').json()
-           curr_page = resp['articles']
-           for article in curr_page:
+           articles = resp['articles']
+           print(len(articles))
+           for article in articles:
                 cleaned_article = clean_text(article['content'])
-                tagged_articles.append(create_tagged_document(cleaned_article, article['id']))
-                ids.append(article['id'])
+                print(type(cleaned_article))
+                tagged_articles.append(create_tagged_document(cleaned_article))
 
-scrape_data()
-
+print(len(tagged_articles))
 # split data
 train_articles, test_articles = train_test_split(tagged_articles, test_size=0.2)
 
 # train the Doc2Vec - need to tweak vector_size hyperparameter
-model = Doc2Vec(vector_size=100, min_count=3, epochs=60, min_alpha=0.001, alpha=0.025)
-model.build_vocab(tagged_articles)
+model = Doc2Vec(vector_size=50, min_count=3, epochs=30)
+model.build_vocab(train_articles)
 model.train(tagged_articles, total_examples=model.corpus_count, epochs=model.epochs)
 
-train_vectors = [model.infer_vector(article.words) for article in train_articles]
-test_vectors = [model.infer_vector(article.words) for article in test_articles]
+# are we just loading the tagged vectors to the db, or should we run like KNN or KMeans ML model?
+train_vectors = [model.infer_vector(article.words) for article, _ in train_articles]
+test_vectors = [model.infer_vector(article.words) for article, _ in test_articles]
 
 ranks = []
 second_ranks = []
-for idx, doc_id in enumerate(train_articles):
-    inferred_vector = model.infer_vector(train_articles[idx].words)
-    sims = model.dv.most_similar([inferred_vector])
-    rank = [docid for docid, sim in sims].index(list(train_articles[idx].tags)[0])
+for doc_id in range(len(train_vectors)):
+    inferred_vector = model.infer_vector(train_vectors[doc_id].words)
+    sims = model.dv.most_similar([inferred_vector], topn=len(model.dv))
+    rank = [docid for docid, sim in sims].index(doc_id)
     ranks.append(rank)
     second_ranks.append(sims[1])
 
